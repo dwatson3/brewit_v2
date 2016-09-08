6da566e1582b6f57917be5176b3995fda8ba57c1
Daphne W <dwatson@newrelic.com>
2016-09-08 10:01:08 -0700

    added superagent

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 
 app.get('/query', function (req, res) {
+  // eventually, we will call a function like getBreweryDb to make an API
+  // request for brew info!
   res.send('your response')
 })
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 // Monogoto
 import React from 'react'
+var request = require('superagent')
 import suitClassNames from 'suitcss-classnames'
+
 require('./App.scss')
 
 export class App extends React.Component {
@@ -14,6 +16,12 @@ export class App extends React.Component {
   }
 
   render () {
+    // TODO: get this working so it calls the express backend.
+    // request
+    //   .get('localhost:3000/query')
+    //   .end(function (err, res) {
+
+    //   })
     const appClasses = this.classNames()
     const titleClasses = this.classNames({descendant: 'title'})
 
